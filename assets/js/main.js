6e61bf3f2440494f4ba851cf0799f9601d95c9f3
Hakan Gezer <hakangezer@Hakans-MBP.fritz.box>
2020-03-16 14:02:12 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+let europäischeUnion = {
+  Belgien: "(BE)",
+  Griechenland: "(EL)",
+  Litauen: "(LT)",
+  Portugal: "(PT)",
+  Bulgarien: "(BG)",
+  Spanien: "(ES)",
+  Luxemburg: "(LU)",
+  Rumänien: "(RO)",
+  Tschechien: "(CZ)",
+  Frankreich: "(FR)",
+  Ungarn: "(HU)",
+  Slowenien: "(SI)",
+  Dänemark: "(DK)",
+  Kroatien: "(HR)",
+  Malta: "(MT)",
+  Slowakei: "(SK)",
+  Deutschland: "(DE)",
+  Italien: "(IT)",
+  Niederlände: "(NL)",
+  Finnland: "(FI)",
+  Estland: "(EE)",
+  Zypern: "(CY)",
+  Österreich: "(AT)",
+  Schweden: "(SE)",
+  Irland: "(IE)",
+  Lettland: "(LV)",
+  Polen: "(PL)",
+  VereinigtesKönigreich: "(UK)"
+};
+console.log(europäischeUnion);
+console.log(europäischeUnion.Belgien);
+console.log(europäischeUnion.Niederlände);
+console.log(europäischeUnion.Luxemburg);
+
+let BeneluxUnion = {
+  Belgien: "(BE)",
+  Niederlände: "(NL)",
+  Luxemburg: "(LU)"
+};
+console.log(BeneluxUnion);
+
+console.log(europäischeUnion);
